7a26ee96566a2b34fff24d6b2ae09c4c2a4d4cee
Cesar Trevino <cdtrevino1@outlook.com>
2023-05-13 12:03:30 -0500

    Added images to product details component.

--- components/products/product-details.js
+++ components/products/product-details.js
@@ -1,6 +1,8 @@
+import Image from "next/image";
+
 // TODO: Include title, price, image(s), add to cart, & product info.
 const ProductDetails = (props) => {
-  const { title, price, description, rating } = props;
+  const { title, price, description, rating, images } = props;
 
   return (
     <>
@@ -8,6 +10,12 @@ const ProductDetails = (props) => {
       <h2>{price}</h2>
       <h3>{description}</h3>
       <h3>{rating}</h3>
+      <Image
+        src={images[0]}
+        alt="Picture of product."
+        width={250}
+        height={250}
+      />
     </>
   );
 };
