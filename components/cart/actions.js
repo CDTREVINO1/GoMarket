c29e53aa5467f7198d9b87f6ca0a5b72c06aba1c
Cesar Trevino <cdtrevino1@outlook.com>
2023-06-30 15:53:01 -0500

    Switched from API Route Handlers to experimental Server Actions.

---
+++ components/cart/actions.js
@@ -0,0 +1,50 @@
+"use server";
+
+import {
+  addToCart,
+  removeFromCart,
+  updateCart,
+} from "../../lib/pos/queries/cart";
+import { cookies } from "next/headers";
+
+export const addItem = async (productId) => {
+  const cartId = cookies().get("cartId")?.value;
+
+  if (!cartId || !productId) {
+    return new Error("Missing cartId or productId");
+  }
+  try {
+    await addToCart(cartId, { productId, quantity: 1 });
+  } catch (e) {
+    return new Error("Error adding item", { cause: e });
+  }
+};
+
+export const removeItem = async (itemId) => {
+  const cartId = cookies().get("cartId")?.value;
+
+  if (!cartId) {
+    return new Error("Missing cartId");
+  }
+  try {
+    await removeFromCart(cartId, itemId);
+  } catch (e) {
+    return new Error("Error removing item", { cause: e });
+  }
+};
+
+export const updateItemQuantity = async ({ itemId, quantity }) => {
+  const cartId = cookies().get("cartId")?.value;
+
+  if (!cartId) {
+    return new Error("Missing cartId");
+  }
+  try {
+    await updateCart(cartId, {
+      itemId,
+      quantity,
+    });
+  } catch (e) {
+    return new Error("Error updating item quantity", { cause: e });
+  }
+};
--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -1,19 +1,62 @@
-import { NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import { getCart, createCart } from "../../../../lib/pos/queries/cart";
-
-export async function GET(req) {
-  const cartId = cookies().get("cartId")?.value;
-
-  const cart = await getCart(cartId);
-
-  // TODO: Return cart.json()
-  return NextResponse.json({ cart });
-}
+import { NextResponse } from "next/server";
+import {
+  createCart,
+  updateCart,
+  removeFromCart,
+} from "../../../../lib/pos/queries/cart";
 
 export async function POST() {
   const cart = await createCart();
   cookies().set("cartId", `${cart._id}`);
 
   return new Response(JSON.stringify(cart));
+}
+
+export async function PUT(req) {
+  const cartId = cookies().get("cartId")?.value;
+  const { quantity, itemId } = await req.json();
+
+  if (!cartId || !quantity || !itemId) {
+    return NextResponse.json(
+      { error: "Missing cartId, itemId, or quantity" },
+      { status: 400 }
+    );
+  }
+  try {
+    await updateCart(cartId, {
+      itemId,
+      quantity,
+    });
+    return NextResponse.json({ status: 204 });
+  } catch (e) {
+    if (e) {
+      return NextResponse.json({ message: e.message }, { status: e.status });
+    }
+
+    return NextResponse.json({ status: 500 });
+  }
+}
+
+export async function DELETE(req) {
+  const cartId = cookies().get("cartId")?.value;
+  const { itemId } = await req.json();
+  console.log("itemId in route: " + itemId);
+
+  if (!cartId || !itemId) {
+    return NextResponse.json(
+      { error: "Missing cartId or itemId" },
+      { status: 400 }
+    );
+  }
+  try {
+    await removeFromCart(cartId, itemId);
+    return NextResponse.json({ status: 204 });
+  } catch (e) {
+    if (e) {
+      return NextResponse.json({ message: e.message }, { status: e.status });
+    }
+
+    return NextResponse.json({ status: 500 });
+  }
 }
