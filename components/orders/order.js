4e83556fbcf03162d98fe570e9baa98e828b0df5
Cesar Trevino <cdtrevino1@outlook.com>
2023-10-06 15:23:21 -0500

    Merged order-history-enhancement

--- components/orders/order.js
+++ components/orders/order.js
@@ -9,22 +9,22 @@ export default function Order({ order }) {
   const { _id, orderTotal, orderItems, createdAt } = order;
 
   return (
-    <>
-      <li>
-        <ul>
-          <li>ORDER ID: {_id}</li>
-          <li>TOTAL: ${orderTotal / 100}</li>
-          <li>ORDER PLACED: {dateFormatter.format(Date.parse(createdAt))}</li>
+    <div className="mb-4 border p-4 shadow-md">
+      <div className="mb-2 flex items-center justify-between">
+        <h2 className="text-lg font-semibold">ORDER ID: {_id}</h2>
+        <p className="text-gray-600">
+          ORDER PLACED: {dateFormatter.format(Date.parse(createdAt))}
+        </p>
+      </div>
+      <p className="mb-2 text-xl font-bold">TOTAL: ${orderTotal / 100}</p>
+      <div className="mb-2">
+        <h3 className="text-lg font-semibold">Products Ordered:</h3>
+        <ul className="list-disc pl-5">
+          {orderItems.map((item, index) => (
+            <OrderItem key={index} item={item} />
+          ))}
         </ul>
-      </li>
-      <li>
-        Products Ordered:
-        <ul>
-          {orderItems.map((item, index) => {
-            return <OrderItem key={index} item={item} />;
-          })}
-        </ul>
-      </li>
-    </>
+      </div>
+    </div>
   );
 }
