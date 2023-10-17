ddf744c39806e79da5789ddf3bd809fb30e5f78f
Cesar Trevino <cdtrevino1@outlook.com>
2023-10-17 13:07:03 -0500

    Merged product-details-design

--- src/app/product/[handle]/page.js
+++ src/app/product/[handle]/page.js
@@ -1,10 +1,10 @@
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import { AddToCart } from "components/cart/add-to-cart";
 import { getProduct } from "lib/pos/queries/product";
 import Footer from "components/layout/footer";
 import Breadcrumb from "components/layout/breadcrumbs";
 import Divider from "components/layout/divider";
+import ProductImage from "./productImage";
 
 export default async function ProductPage({ params }) {
   const product = await getProduct(params.handle);
@@ -21,67 +21,46 @@ export default async function ProductPage({ params }) {
     <>
       <Breadcrumb links={breadcrumbs} />
 
-      <div className="m-auto max-w-3xl md:flex">
-        {/* Product Image */}
-        <div className="w-full ">
-          <div className="p-4">
-            <Image
-              src={product.images[0]}
-              alt="Picture of product."
-              height={500}
-              width={500}
-              className="w-full rounded-lg object-center"
-            />
+      <div className="mx-auto max-w-5xl">
+        <div className="flex flex-col md:flex-row">
+          <div className="mx-4 border md:mx-auto md:w-1/2">
+            <ProductImage product={product} />
           </div>
-        </div>
 
-        {/* Product Details */}
-        <div className="pl-2 ">
-          <div>
-            <h1 className="ml-4 text-4xl tracking-tight text-gray-900">
-              {product.name}
-            </h1>
-            <h1 className="ml-4 text-4xl tracking-tight text-gray-900">
-              {product.brand}
-            </h1>
-            <div className="mt-3">
-              <h2 className="sr-only">Product information</h2>
-              <p className="mb-4 ml-4 text-lg tracking-tight text-gray-900">
-                ${product.price}
+          {/* Product Details */}
+          <div className="mx-4 flex max-w-lg flex-col justify-between bg-gray-100 p-6 md:w-1/2">
+            {/* Product Title and Brand */}
+            <div>
+              <h1 className="ml-2 mt-8 text-4xl font-bold tracking-tight text-gray-900 ">
+                {product.name}
+              </h1>
+
+              {/* Product Price */}
+              <div className="mt-3">
+                <h2 className="sr-only">Product information</h2>
+                <p className="mb-4 ml-2 mt-8 text-2xl font-thin tracking-tight text-gray-900">
+                  ${product.price}
+                </p>
+              </div>
+
+              <Divider />
+
+              {/* Product Description */}
+              <p className="ml-2 mt-8 text-lg text-gray-600">
+                {product.description}
               </p>
             </div>
-            <div className="items-center text-yellow-500">
-              {Array.from({ length: product.rating }, (_, index) => (
-                <svg
-                  key={index}
-                  className="h-5 w-5 fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20">
-                  <path
-                    fillRule="evenodd"
-                    d="M10 1.13l1.54 4.63h4.87a.5.5 0 01.36.86l-3.92 2.84 1.5 4.5a.5.5 0 01-.76.57L10 12.83l-3.09 2.5a.5.5 0 01-.76-.57l1.5-4.5-3.92-2.84a.5.5 0 01.36-.86h4.87L10 1.13z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              ))}
+
+            {/* Add to Cart */}
+            <div className="mt-auto">
+              <AddToCart
+                availableForSale={product.availability}
+                productId={product._id}
+              />
             </div>
-
-            <Divider />
-
-            <p className="ml-4 mt-2 text-lg text-gray-600">
-              {product.description}
-            </p>
-          </div>
-          {/* Add to Cart */}
-          <div className="mx-auto mb-10 mt-20 ">
-            <AddToCart
-              availableForSale={product.availability}
-              productId={product._id}
-            />
           </div>
         </div>
       </div>
-
       <Footer />
     </>
   );
---
+++ src/app/product/[handle]/productImage.js
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+
+function ProductImage({ product }) {
+  const [selectedImage, setSelectedImage] = useState(product.images[0]);
+
+  const handleImageClick = (image) => {
+    setSelectedImage(image);
+  };
+
+  return (
+    <div className="m-auto max-w-3xl p-2 md:flex">
+      <div className="w-full">
+        <div className="">
+          {/* Main Image */}
+          <div className="mb-4">
+            <Image
+              src={selectedImage}
+              alt={`Picture of product`}
+              height={500}
+              width={500}
+              className="w-full rounded-lg object-center"
+            />
+          </div>
+          {/* Thumbnail Images */}
+          <div className="flex ">
+            {product.images.map((image, index) => (
+              <div
+                key={index}
+                className="mr-2 cursor-pointer"
+                onClick={() => handleImageClick(image)}>
+                <Image
+                  src={image}
+                  alt={`Thumbnail of product - ${index}`}
+                  height={100}
+                  width={100}
+                  className={`rounded-lg object-center ${
+                    selectedImage === image ? "border-2 border-blue-500" : ""
+                  }`}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductImage;
