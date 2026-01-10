ac56e141109660053fc6dce58c1acfec372e38ff
CDTREVINO1 <cdtrevino1@outlook.com>
2026-01-10 10:55:13 -0600

    Fix code where MongoDB product id was not being added when a stripe product was created. Fix product filtering for admin ProductTable component.

--- components/products/products-table.tsx
+++ components/products/products-table.tsx
@@ -1,86 +1,115 @@
 "use client"
 
 import { useState } from "react"
-
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table"
 import type { Prisma } from "@/generated/prisma/browser"
+
+import { Button } from "@/components/ui/button"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 import ArchiveProductDialog from "./archive-product-dialog"
 import EditProductSheet from "./edit-product-sheet"
 
 type Product = Prisma.ProductModel
 
-export default function ProductsTable({
-    products,
-}: {
-    products: Product[]
-}) {
-    const [filter, setFilter] = useState("all")
+export default function ProductsTable({ products }: { products: Product[] }) {
+  const [filter, setFilter] = useState("all")
 
-    const dateFormatter = new Intl.DateTimeFormat(undefined, {
-        dateStyle: "short",
-        timeStyle: "short",
-    })
+  const dateFormatter = new Intl.DateTimeFormat(undefined, {
+    dateStyle: "short",
+    timeStyle: "short",
+  })
 
-    const filteredProducts = () => {
-        if (filter === "available") {
-            return products.filter((product) => product.availability === true)
-        } else if (filter === "archived") {
-            return products.filter((product) => product.availability === false)
-        } else return products
-    }
+  const filteredProducts = () => {
+    if (filter === "available") {
+      return products.filter((product) => product.availability === true)
+    } else if (filter === "archived") {
+      return products.filter((product) => product.availability === false)
+    } else return products
+  }
 
-    if (!products || products.length === 0) {
-        return (
-            <div className="product-list-placeholder">
-                <p className="text-gray-500">No products available at the moment.</p>
-            </div>
-        )
-    }
-
+  if (!products || products.length === 0) {
     return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Title</TableHead>
-                    <TableHead>Price</TableHead>
-                    <TableHead>Date Created</TableHead>
-                    <TableHead>Date Updated</TableHead>
-                    <TableHead />
-                    <TableHead />
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {filteredProducts()?.length === 0 && (
-                    <TableRow>
-                        <TableCell>No products matched for criteria.</TableCell>
-                    </TableRow>
-                )}
-                {filteredProducts()?.map((product) => (
-                    <TableRow key={product.id}>
-                        <TableCell>{product.title}</TableCell>
-                        <TableCell>${product.price}</TableCell>
-                        <TableCell>{dateFormatter.format(product.createdAt)}</TableCell>
-                        <TableCell>{dateFormatter.format(product.updatedAt)}</TableCell>
-                        <TableCell>
-                            <EditProductSheet product={product} />
-                        </TableCell>
-                        <TableCell>
-                            <ArchiveProductDialog
-                                productId={product.id}
-                                isAvailable={product.availability}
-                            />
-                        </TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+      <div className="product-list-placeholder">
+        <p className="text-gray-500">No products available at the moment.</p>
+      </div>
     )
+  }
+
+  return (
+    <>
+      <Button
+        onClick={() => setFilter("all")}
+        className={`mr-4 rounded-lg border border-blue-500 px-8 py-2 text-center shadow-md focus:outline-none ${
+          filter === "all"
+            ? "bg-primary text-secondary"
+            : "bg-secondary text-primary hover:bg-blue-50"
+        } `}
+      >
+        All
+      </Button>
+      <Button
+        onClick={() => setFilter("available")}
+        className={`mr-4 rounded-lg border border-blue-500 px-4 py-2 text-center shadow-md focus:outline-none ${
+          filter === "available"
+            ? "bg-primary text-secondary"
+            : "bg-secondary text-primary hover:bg-blue-50"
+        } `}
+      >
+        Available
+      </Button>
+      <Button
+        onClick={() => setFilter("archived")}
+        className={`rounded-lg border border-blue-500 px-4 py-2 text-center shadow-md focus:outline-none ${
+          filter === "archived"
+            ? "bg-primary text-secondary"
+            : "bg-secondary text-primary hover:bg-blue-50"
+        } `}
+      >
+        Archived
+      </Button>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Title</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Date Created</TableHead>
+            <TableHead>Date Updated</TableHead>
+            <TableHead />
+            <TableHead />
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {filteredProducts()?.length === 0 && (
+            <TableRow>
+              <TableCell>No products matched for criteria.</TableCell>
+            </TableRow>
+          )}
+          {filteredProducts()?.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{product.title}</TableCell>
+              <TableCell>${product.price}</TableCell>
+              <TableCell>{dateFormatter.format(product.createdAt)}</TableCell>
+              <TableCell>{dateFormatter.format(product.updatedAt)}</TableCell>
+              <TableCell>
+                <EditProductSheet product={product} />
+              </TableCell>
+              <TableCell>
+                <ArchiveProductDialog
+                  productId={product.id}
+                  isAvailable={product.availability}
+                />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
+  )
 }
--- lib/pos/mutations/product.ts
+++ lib/pos/mutations/product.ts
@@ -4,347 +4,240 @@ import { serverEnv } from "@/env/server"
 import prisma from "@/lib/prisma"
 
 type Image = {
-    public_id: string
-    url: string
+  public_id: string
+  url: string
 }
 
 type CreateProductInput = {
-    title: string
-    description: string
-    price: number
-    category: string
-    images: Image[]
-    handle: string
-    availability: boolean
+  title: string
+  description: string
+  price: number
+  category: string
+  images: Image[]
+  handle: string
+  availability: boolean
 }
 
 type UpdateProductInput = {
-    id: string
-    title?: string
-    description?: string
-    price?: number
-    category?: string
-    images?: Image[]
-    handle?: string
-    availability?: boolean
+  id: string
+  title?: string
+  description?: string
+  price?: number
+  category?: string
+  images?: Image[]
+  handle?: string
+  availability?: boolean
 }
 
 export const createProduct = async (productData: CreateProductInput) => {
-    try {
-        const stripe = new Stripe(serverEnv.STRIPE_SECRET_KEY)
+  try {
+    const stripe = new Stripe(serverEnv.STRIPE_SECRET_KEY)
 
-        const stripeProduct = await stripe.products.create({
-            name: productData.title,
-            description: productData.description,
-            images: productData.images.map((img) => img.url),
-            metadata: {
-                category: productData.category,
-                handle: productData.handle,
-            },
-        })
+    const product = await prisma.product.create({
+      data: {
+        title: productData.title,
+        description: productData.description,
+        price: productData.price,
+        category: productData.category,
+        images: productData.images,
+        handle: productData.handle,
+        availability: productData.availability,
+      },
+    })
 
-        // 2. Create price in Stripe
-        const stripePrice = await stripe.prices.create({
-            product: stripeProduct.id,
-            unit_amount: Math.round(productData.price * 100), // Convert to cents
-            currency: "usd",
-            tax_behavior: "exclusive",
-        })
+    const stripeProduct = await stripe.products.create({
+      id: product.id,
+      tax_code: "txcd_99999999",
+      name: productData.title,
+      description: productData.description,
+      images: productData.images.map((img) => img.url),
+      metadata: {
+        category: productData.category,
+        handle: productData.handle,
+      },
+    })
 
-        const product = await prisma.product.create({
-            data: {
-                title: productData.title,
-                description: productData.description,
-                price: productData.price,
-                category: productData.category,
-                images: productData.images,
-                handle: productData.handle,
-                availability: productData.availability,
-                stripePriceId: stripePrice.id,
-            },
-        })
+    // 2. Create price in Stripe
+    const stripePrice = await stripe.prices.create({
+      product: stripeProduct.id,
+      unit_amount: Math.round(productData.price * 100), // Convert to cents
+      currency: "usd",
+      tax_behavior: "exclusive",
+    })
 
-        return {
-            success: true,
-            product,
-            stripeProduct,
-            stripePrice,
-        }
+    await prisma.product.update({
+      where: {
+        id: product.id,
+      },
+      data: {
+        stripePriceId: stripePrice.id,
+      },
+    })
 
-        // const handle = title.toLowerCase().replaceAll(" ", "-")
-
-        // const newProduct = { title, price, handle }
-
-        // if (description) newProduct.description = description
-        // if (category) newProduct.category = category
-        // if (images) newProduct.images = images
-
-        // const product = await prisma.product.create(newProduct)
-        // console.log(product)
-
-        // newProduct.id = product._id.toString()
-        // newProduct.tax_code = "txcd_99999999"
-        // delete newProduct.price
-        // delete newProduct.handle
-        // delete newProduct.category
-        // delete newProduct.images
-
-        // // Tax code is currently set to General - Tangible Goods refer to Stripe
-        // // Docs for different codes https://stripe.com/docs/tax/tax-codes
-        // const stripeProduct = await stripe.products.create(newProduct)
-        // console.log(stripeProduct)
-
-        // const stripePrice = await stripe.prices.create({
-        //   product: stripeProduct.id,
-        //   unit_amount: Math.round(product.price * 100),
-        //   currency: "usd",
-        //   tax_behavior: "exclusive",
-        // })
-        // console.log(stripePrice)
-
-        // product.stripePriceId = stripePrice.id
-        // await product.save()
-    } catch (error) {
-        console.log("Error creating product:", error)
-        throw error
+    return {
+      success: true,
+      product,
+      stripeProduct,
+      stripePrice,
     }
+  } catch (error) {
+    console.log("Error creating product:", error)
+    throw error
+  }
 }
 
 export const updateProduct = async (productData: UpdateProductInput) => {
-    try {
-        const stripe = new Stripe(serverEnv.STRIPE_SECRET_KEY)
+  try {
+    const stripe = new Stripe(serverEnv.STRIPE_SECRET_KEY)
 
-        const existingProduct = await prisma.product.findUnique({
-            where: { id: productData.id },
-        })
+    const existingProduct = await prisma.product.findUnique({
+      where: { id: productData.id },
+    })
 
-        if (!existingProduct) {
-            throw new Error("Product not found")
-        }
-
-        if (
-            productData.title ||
-            productData.description ||
-            productData.images ||
-            productData.category ||
-            productData.handle
-        ) {
-            // First, get the Stripe product ID from the price
-            const stripePrice = await stripe.prices.retrieve(
-                existingProduct.stripePriceId
-            )
-            const stripeProductId = stripePrice.product as string
-
-            await stripe.products.update(stripeProductId, {
-                ...(productData.title && { name: productData.title }),
-                ...(productData.description && {
-                    description: productData.description,
-                }),
-                ...(productData.images && {
-                    images: productData.images.map((img) => img.url),
-                }),
-                ...(productData.category || productData.handle
-                    ? {
-                        metadata: {
-                            ...(productData.category && { category: productData.category }),
-                            ...(productData.handle && { handle: productData.handle }),
-                        },
-                    }
-                    : {}),
-            })
-        }
-
-        let stripePriceId = existingProduct.stripePriceId
-        if (productData.price && productData.price !== existingProduct.price) {
-            // Get the Stripe product ID
-            const oldStripePrice = await stripe.prices.retrieve(
-                existingProduct.stripePriceId
-            )
-            const stripeProductId = oldStripePrice.product as string
-
-            // Create new price
-            const newStripePrice = await stripe.prices.create({
-                product: stripeProductId,
-                unit_amount: Math.round(productData.price * 100),
-                currency: "usd",
-            })
-
-            // Archive old price
-            await stripe.prices.update(existingProduct.stripePriceId, {
-                active: false,
-            })
-
-            stripePriceId = newStripePrice.id
-        }
-
-        const updatedProduct = await prisma.product.update({
-            where: { id: productData.id },
-            data: {
-                ...(productData.title && { title: productData.title }),
-                ...(productData.description && {
-                    description: productData.description,
-                }),
-                ...(productData.price && { price: productData.price }),
-                ...(productData.category && { category: productData.category }),
-                ...(productData.images && { images: productData.images }),
-                ...(productData.handle && { handle: productData.handle }),
-                ...(productData.availability !== undefined && {
-                    availability: productData.availability,
-                }),
-                ...(stripePriceId !== existingProduct.stripePriceId && {
-                    stripePriceId,
-                }),
-            },
-        })
-
-        return {
-            success: true,
-            product: updatedProduct,
-        }
-
-        // const { id, title, description, category, price, images } = product
-
-        // const existingProduct = await prisma.product.findUnique({
-        //   where: {
-        //     id: id,
-        //   },
-        // })
-
-        // const updatedFields = {}
-        // if (title !== existingProduct.title) updatedFields.title = title
-        // if (description !== existingProduct.description)
-        //   updatedFields.description = description
-        // if (category !== existingProduct.category) updatedFields.category = category
-        // if (price !== existingProduct.price) updatedFields.price = price
-
-        // if (existingProduct.images.length === 0 && images)
-        //   updatedFields.images = images
-
-        // if (existingProduct.images.length > 0 && images) {
-        //   const mergedImages = [...existingProduct.images, ...images]
-        //   updatedFields.images = mergedImages
-        // }
-
-        // if (Object.keys(updatedFields).length > 0) {
-        //   const updatedProduct = await prisma.product.update({
-        //     where: { id: id },
-        //     data: {
-        //       ...updatedFields,
-        //     },
-        //   })
-
-        //   if (updatedFields.price) {
-        //     await stripe.prices.update(updatedProduct.stripePriceId, {
-        //       active: false,
-        //     })
-
-        //     const newPrice = await stripe.prices.create({
-        //       product: id,
-        //       unit_amount: Math.round(price * 100),
-        //       currency: "usd",
-        //     })
-        //     console.log("New Stripe Price created:" + newPrice)
-
-        //     await stripe.products.update(id, {
-        //       name: title,
-        //       description: description,
-        //       default_price: newPrice.id,
-        //     })
-
-        //     updatedProduct.stripePriceId = newPrice.id
-        //     updatedProduct.save()
-        //   }
-
-        //   if (updatedFields.description) {
-        //     await stripe.products.update(id, {
-        //       description: updatedFields.description,
-        //     })
-        //   }
-
-        //   console.log(updatedProduct)
-        //   if (!updatedProduct) throw new Error("Product not found.")
-        // }
-    } catch (error) {
-        console.log("Error updating product:", error)
-        throw new Error("Failed to update product")
+    if (!existingProduct) {
+      throw new Error("Product not found")
     }
+
+    if (
+      productData.title ||
+      productData.description ||
+      productData.images ||
+      productData.category ||
+      productData.handle
+    ) {
+      // First, get the Stripe product ID from the price
+      const stripePrice = await stripe.prices.retrieve(
+        existingProduct.stripePriceId
+      )
+      const stripeProductId = stripePrice.product as string
+
+      await stripe.products.update(stripeProductId, {
+        ...(productData.title && { name: productData.title }),
+        ...(productData.description && {
+          description: productData.description,
+        }),
+        ...(productData.images && {
+          images: productData.images.map((img) => img.url),
+        }),
+        ...(productData.category || productData.handle
+          ? {
+              metadata: {
+                ...(productData.category && { category: productData.category }),
+                ...(productData.handle && { handle: productData.handle }),
+              },
+            }
+          : {}),
+      })
+    }
+
+    let stripePriceId = existingProduct.stripePriceId
+    if (productData.price && productData.price !== existingProduct.price) {
+      // Get the Stripe product ID
+      const oldStripePrice = await stripe.prices.retrieve(
+        existingProduct.stripePriceId
+      )
+      const stripeProductId = oldStripePrice.product as string
+
+      // Create new price
+      const newStripePrice = await stripe.prices.create({
+        product: stripeProductId,
+        unit_amount: Math.round(productData.price * 100),
+        currency: "usd",
+      })
+
+      // Archive old price
+      await stripe.prices.update(existingProduct.stripePriceId, {
+        active: false,
+      })
+
+      stripePriceId = newStripePrice.id
+    }
+
+    const updatedProduct = await prisma.product.update({
+      where: { id: productData.id },
+      data: {
+        ...(productData.title && { title: productData.title }),
+        ...(productData.description && {
+          description: productData.description,
+        }),
+        ...(productData.price && { price: productData.price }),
+        ...(productData.category && { category: productData.category }),
+        ...(productData.images && { images: productData.images }),
+        ...(productData.handle && { handle: productData.handle }),
+        ...(productData.availability !== undefined && {
+          availability: productData.availability,
+        }),
+        ...(stripePriceId !== existingProduct.stripePriceId && {
+          stripePriceId,
+        }),
+      },
+    })
+
+    return {
+      success: true,
+      product: updatedProduct,
+    }
+  } catch (error) {
+    console.log("Error updating product:", error)
+    throw new Error("Failed to update product")
+  }
 }
 
 export const archiveProduct = async (
-    productId: string,
-    isAvailable: boolean
+  productId: string,
+  isAvailable: boolean
 ) => {
-    try {
-        const stripe = new Stripe(serverEnv.STRIPE_SECRET_KEY)
+  try {
+    const stripe = new Stripe(serverEnv.STRIPE_SECRET_KEY)
 
-        const productToArchive = await prisma.product.update({
-            where: { id: productId },
-            data: {
-                availability: !isAvailable,
-            },
-        })
+    const productToArchive = await prisma.product.update({
+      where: { id: productId },
+      data: {
+        availability: !isAvailable,
+      },
+    })
 
-        if (productToArchive.stripePriceId) {
-            await stripe.products.update(productToArchive.id.toString(), {
-                active: !isAvailable,
-            })
-            await stripe.prices.update(productToArchive.stripePriceId, {
-                active: !isAvailable,
-            })
-        }
-
-        console.log(productToArchive)
-    } catch (error) {
-        console.log("Error archiving product:", error)
-        throw error
+    if (productToArchive.stripePriceId) {
+      await stripe.products.update(productToArchive.id.toString(), {
+        active: !isAvailable,
+      })
+      await stripe.prices.update(productToArchive.stripePriceId, {
+        active: !isAvailable,
+      })
     }
+  } catch (error) {
+    console.log("Error archiving product:", error)
+    throw error
+  }
 }
 
 export const deleteImage = async (productId: string, imageId: string) => {
-    try {
-        const product = await prisma.product.findUnique({
-            where: { id: productId },
-            select: { images: true },
-        })
+  try {
+    const product = await prisma.product.findUnique({
+      where: { id: productId },
+      select: { images: true },
+    })
 
-        if (!product) {
-            throw new Error("Product not found")
-        }
-
-        const currentImages = product.images as Image[]
-        const updatedImages = currentImages.filter(
-            (img) => img.public_id !== imageId
-        )
-
-        // Update the product with the filtered images
-        const updatedProduct = await prisma.product.update({
-            where: { id: productId },
-            data: {
-                images: updatedImages,
-            },
-        })
-
-        console.log(updatedProduct)
-        return updatedProduct
-
-        // const updatedProduct = await prisma.product.update({
-        //   where: { id: productId },
-        //   data: {
-        //     images: {
-        //       set:
-        //         (
-        //           await prisma.product.findUnique({
-        //             where: { id: productId },
-        //             select: { images: true },
-        //           })
-        //         )?.images.filter((img) => img !== imageId) ?? [],
-        //     },
-        //   },
-        // })
-
-        // console.log(updatedProduct)
-    } catch (error) {
-        console.log("Error deleting image:", error)
-        throw error
+    if (!product) {
+      throw new Error("Product not found")
     }
+
+    const currentImages = product.images as Image[]
+    const updatedImages = currentImages.filter(
+      (img) => img.public_id !== imageId
+    )
+
+    // Update the product with the filtered images
+    const updatedProduct = await prisma.product.update({
+      where: { id: productId },
+      data: {
+        images: updatedImages,
+      },
+    })
+
+    return updatedProduct
+  } catch (error) {
+    console.log("Error deleting image:", error)
+    throw error
+  }
 }
